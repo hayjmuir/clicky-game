ceaeb4f613f8686b58143072a1b65a2d547bac1a
hayjmuir <haymuir@gmail.com>
2020-06-22 21:37:11 -0700

    Updates

---
+++ precache-manifest.623244b4805e53e1ac535fd29e9f5c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8e53b8379c6e388ecb0caf02600ed4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "ce108050a0d534109e46",
+    "url": "/clicky-game/static/css/main.9334b262.chunk.css"
+  },
+  {
+    "revision": "6ce6f54f724085dc1c01",
+    "url": "/clicky-game/static/js/2.93eb1781.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.93eb1781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce108050a0d534109e46",
+    "url": "/clicky-game/static/js/main.5817b98b.chunk.js"
+  },
+  {
+    "revision": "ea8de61c71fd36f495fc",
+    "url": "/clicky-game/static/js/runtime-main.596060ab.js"
+  }
+]);
